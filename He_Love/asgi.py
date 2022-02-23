7080ccc89905bd6a581c9fc76df5f73dd4e4bd1c
Lenny Boegli <lenny.boegli@he-arc.ch>
2022-02-23 09:13:32 +0100

    Django project init

---
+++ He_Love/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for He_Love project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'He_Love.settings')
+
+application = get_asgi_application()
