e43e5c3e81c89b2dbabdfed66410bf2707507389
Luca Campana <luca.campana@he-arc.ch>
2022-03-09 09:44:39 +0100

    Tested routing --> works for users & created events  routing --> needs templates

--- He_Loveapp/urls.py
+++ He_Loveapp/urls.py
@@ -8,4 +8,12 @@ urlpatterns = [
     path('users/create/', views.UserCreateView.as_view(), name='users-create'),
     path('users/<pk>/', views.UserDetailView.as_view(), name='users-detail'),
     path('users/<pk>/update/', views.UserUpdateView.as_view(), name='users-update'),
+
+    path('events/', views.EventListView.as_view(), name='events-list'),
+    path('events/create/', views.EventCreateView.as_view(), name='events-create'),
+    path('events/<pk>/', views.EventDetailView.as_view(), name='events-detail'),
+    path('events/<pk>/update/', views.EventUpdateView.as_view(), name='events-update'),
+    path('events/<pk>/delete/', views.EventDeleteView.as_view(), name='events-delete'),
+
+
 ]
