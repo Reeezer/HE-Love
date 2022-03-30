0e2b3d1a1624a860f603b874cfcdcd5cbc8faa5c
LennyBo <lenny.boegli@moosvolk.ch>
2022-03-30 09:01:58 +0200

    Sysadmin life

--- He_Loveapp/models.py
+++ He_Loveapp/models.py
@@ -149,7 +149,7 @@ class Match(models.Model):
 class Chat(models.Model):
     user_sender = models.ForeignKey(AppUser, on_delete=models.CASCADE, related_name='chat_user_sender')
     user_receiver = models.ForeignKey(AppUser, on_delete=models.CASCADE, related_name='chat_user_receiver')
-    match = models.ForeignKey(Match, on_delete=models.CASCADE, related_name='chat_match',default=None)
+    match = models.ForeignKey(Match, on_delete=models.CASCADE, related_name='chat_match')
     message = models.TextField()
     date = models.DateTimeField(default=datetime.datetime.now)
     
