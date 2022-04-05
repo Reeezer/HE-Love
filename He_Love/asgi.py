6d52493d39ece7109e0f1a7f9936680e4c081ad8
Luca Campana <luca.campana@he-arc.ch>
2022-04-06 00:06:14 +0200

    Update asgi.py

--- He_Love/asgi.py
+++ He_Love/asgi.py
@@ -3,11 +3,14 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'He_Love.settings')
+
 import django
 django.setup()
 import He_Loveapp.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'He_Love.settings')
+
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
